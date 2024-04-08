626c02eff556c23e0b66fc710e7ce0e1fc3ae160
vishaly99 <81356194+vishaly99@users.noreply.github.com>
2024-04-08 12:23:06 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//let score=null
+//let score="33abc"
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn=0
+//let isLoggedIn="";
+let isLoggedIn="asd";
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// "" => false
+// 1 => true 0 => false 
+// "hitesh" => true

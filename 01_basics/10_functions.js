15738f6bbecc8cd0797cf711302dfc26c65a0d66
vishaly99 <81356194+vishaly99@users.noreply.github.com>
2024-04-19 07:34:25 +0000

    fucntions

---
+++ 01_basics/10_functions.js
@@ -0,0 +1,50 @@
+function SayMyName(){
+    console.log("V");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("L");
+}
+//SayMyName()
+
+// function addTwoNumbers(number1,number2){
+//     //console.log(number1+number2)
+//     return number1+number2
+// }
+//addTwoNumbers(1,2)
+//addTwoNumbers(1,"a")
+
+// const result=addTwoNumbers(1,2)
+// console.log(result)
+
+// console.log("Result:",result)
+
+// function logininUsermessage(username="sam"){
+//     if(!username){
+//         console.log("Please enter a message")
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(logininUsermessage("hitesh"))
+// console.log(logininUsermessage())
+
+//... rest operator used in function only used to print multiple values
+// function calculatecartprice(...num1){
+//     return num1
+// }
+
+//console.log(calculatecartprice(100,200,300))
+
+// function calculatecartprice(val1,val2,...num1){
+//     console.log(num1)
+//     return val1+" "+val2+" "+ num1
+// }
+// console.log(calculatecartprice(100,200,300,400))
+
+const user={
+    username:"hitesh",
+    prices
+}
